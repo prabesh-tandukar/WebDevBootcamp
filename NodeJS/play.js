20103409ea9d8e886f4907c21d02827f48a490bf
Prabesh <tandukarprabesh@gmail.com>
2024-09-08 23:18:46 +1200

    node

---
+++ NodeJS/play.js
@@ -0,0 +1,16 @@
+const name = "Max";
+let age = 29;
+const hasHobbies = true;
+
+age = 30;
+
+const summarizeUser = (userName, userAge, userHasHobby) => {
+  return (
+    "Name is " +
+    name +
+    "age is " +
+    age +
+    "and the user has hobbies which is " +
+    hasHobbies
+  );
+};
